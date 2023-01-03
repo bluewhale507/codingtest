a01ac7dd7ce11498ba0fdbe191e7ff1701c5acb5
bluewhale507 <jec9663@gmail.com>
2023-01-04 00:55:30 +0900

    [옹알이(1)_0lv]

---
+++ programers/babbling_1/Solution.java
@@ -0,0 +1,30 @@
+package programers.babbling_1;
+
+class Solution {
+
+    public static String[] arr = {"aya", "ye", "woo", "ma"};
+
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        for(String babble : babbling) {
+            for(String e : arr) {
+               babble = babble.replace(e," ");
+            }
+            if(babble.isBlank()) answer++;
+        }
+        return answer;
+    }
+}
+
+class SolutionEx {
+    public static void main(String[] args) {
+
+        String[] test1 = {"aya", "yee", "u", "maa", "wyeoo"};
+        String[] test2 = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(test1));
+    }
+}
